93664f49304f9b58687ef8f6889a672b43989893
AqingCyan <aqingcyan@cyanthing.com>
2020-01-26 22:56:42 +0800

    🧩 查询文件元信息接口

--- mian.go
+++ mian.go
@@ -9,6 +9,7 @@ import (
 func main()  {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		fmt.Printf("Failed to start Server, err:%s", err.Error())
