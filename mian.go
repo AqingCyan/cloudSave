ec7c72ebb6f3c751a87522db267c8d2c92355199
AqingCyan <aqingcyan@cyanthing.com>
2020-01-26 20:41:47 +0800

    :tada: 毕业设计开工

---
+++ mian.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"filestore-server/handler"
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Printf("Failed to start Server, err:%s", err)
+	}
+}
