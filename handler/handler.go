483d1144bd8e3c42e7e355d486fc4f1e01e41814
AqingCyan <aqingcyan@cyanthing.com>
2020-01-26 21:55:54 +0800

    🪐 完成文件上传接口

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // UploadHandler: 处理文件上传
@@ -18,6 +20,30 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		_, _ = io.WriteString(w, string(data))
 	} else if r.Method == "POST" {
 		// 接收文件流及存储到本地目录
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get Data, err:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
 
+		newFile, err := os.Create("/tmp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create file, err:%s", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save data into file, err:%s", err.Error())
+			return
+		}
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
+}
+
+// UploadSucHandler: 上传已完成
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	_, _ = io.WriteString(w, "Upload finished")
 }
--- mian.go
+++ mian.go
@@ -8,8 +8,9 @@ import (
 
 func main()  {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
-		fmt.Printf("Failed to start Server, err:%s", err)
+		fmt.Printf("Failed to start Server, err:%s", err.Error())
 	}
 }
